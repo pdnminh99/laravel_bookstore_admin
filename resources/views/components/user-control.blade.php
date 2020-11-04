0e9503797946cb11a907fba8971325ce68cd5004
pdnminh99 <pdn.minh1999@gmail.com>
2020-11-04 08:24:47 +0700

    replace default user profile pic to konoha symbol

--- resources/views/components/user-control.blade.php
+++ resources/views/components/user-control.blade.php
@@ -4,7 +4,7 @@
            aria-expanded="false">
             <div class="media align-items-center">
                 <span class="avatar avatar-sm rounded-circle">
-                    <img alt="Image placeholder" src="{{ asset('img/theme/team-4.jpg') }}">
+                    <img alt="Image placeholder" src="{{ asset('img/icons/konoha.jpg') }}">
                   </span>
                 <div class="media-body  ml-2  d-none d-lg-block">
                     <span class="mb-0 text-sm  font-weight-bold">{{ $username ?? 'NULL' }}</span>
